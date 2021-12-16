7ea4766f6823c4df5f2e06021d7b7a80ffe643d3
shubham dhama <dhamashubham107@gmail.com>
2021-12-16 15:46:41 +0530

    returning the response

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const port =8000;
 
-const app= express();
+const app= express(); //app is used as naming convention.
+
+// app.get() to return the response
+
+app.get('/',function(req,res){
+    // we dont need to set the content-type. it is automactically done my express
+    res.send('<h1>hi i think the page is rendered</h1>'); 
+
+    // here instead of end() we use send().
+});
 
 
-
-
-
+//app.listen() to listen to the request
 
 app.listen(port,function(err){
     if(err){
         console.log('error in running server');
 
     }
-    console.log('my express server is running on port:',port);
+    console.log('my express server is running on port no:',port);
 })
